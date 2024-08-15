dba4139646ddaa36bcf44db0264b2327a2cd0a8c
Samrudh Sivva <samrudh5000@gmail.com>
2024-08-14 22:18:31 -0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-const GOOGLE_API_KEY='AIzaSyByCkuojhh5ZPPfYMmOYJV5SlyHcMUGqlY'
+const GOOGLE_API_KEY=''
 
 export const YOUTUBE_API_POPULAR_VIDEOS= 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key='+GOOGLE_API_KEY;
